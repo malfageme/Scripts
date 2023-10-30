70b2fc9eda86c76d0f1d9b1ebb42a69e34bb9cd0
malfageme <malfageme@gmail.com>
2023-10-30 08:34:48 -0700

    delete

--- Networking/capture_unl.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#echo $1 >> /tmp/capture.log
-
-HOST=`echo $1 | awk -F"://" '{print $2}' | cut -d/ -f1`
-PORT=`echo $1 | awk -F"://" '{print $2}' | cut -d/ -f2 | sed 's/vunl_/vunl0_/'`
-
-echo "Capturing on: Host: $HOST  -   Port: $PORT"  >> /tmp/capture.log
-
-xterm -e "wireshark -k -i <(ssh -l root $HOST tcpdump -s 0 -U -n -v -w - -i $PORT)"
